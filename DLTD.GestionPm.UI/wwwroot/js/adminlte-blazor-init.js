aa8ed1e030710a92f0438b771ee0a0c6d39aace3
Deimer Castrillo Martinez <da.cm.10@hotmail.com>
2025-10-09 07:50:54 -0500

    Admilte Menuvertical No funciona

---
+++ DLTD.GestionPm.UI/wwwroot/js/adminlte-blazor-init.js
@@ -0,0 +1,9 @@
+﻿
+
+window.initAdminLteSidebar = () => {
+    setTimeout(() => {
+        if (window.AdminLTE && window.AdminLTE.Sidebar) {
+            window.AdminLTE.Sidebar._jQueryInterface.call(document.querySelectorAll('.app-sidebar'));
+        }
+    }, 100);
+};
